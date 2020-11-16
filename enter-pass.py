d4ca77024f7de7d5e7b2cd565bd8dce8fa034317
mohanad70 <mmmsssaaa333@gmail.com>
2020-11-16 06:12:13 +0300

    Enter-Password

---
+++ enter-pass.py
@@ -0,0 +1,15 @@
+print('Who are you ..?')
+count=0
+
+while count < 3:
+	username=input('Enter the username:')
+	password=input('Enter the password:')
+	if username== 'mohanad' and  password=='123456':
+		print('Welcome Back Mohanad ..')
+		break
+	elif count < 2:
+		print('Access denied Try again..')
+		count+=1
+	elif count <3:
+		print('you have been baned')
+		break
